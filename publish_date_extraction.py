56f2581add5249294ee37dd5242754ae7ecc2c9f
DecongYu <49385992+DecongYu@users.noreply.github.com>
2020-07-14 21:19:33 -0600

    Create publish_date_extraction.py
    draft

---
+++ publish_date_extraction.py
@@ -0,0 +1,38 @@
+import csv
+import re
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+df = pd.read_csv("ccmf_data_13Jul2020.csv")
+
+webs = df['article_url'] # 
+
+# Publish date pattern 'July 3, 2019' or 'Jul 03, 2019'
+pattern = re.compile(r'(Jan\w*|Feb\w*|Mar\w*|May|Jun\w*|Jul\w*|Aug\w*|Sep\w*|Oct\w*|Nov\w*|Dec\w*)\s(\d{1,2}),\s20\d{2}')
+
+dates_published = [] # create an empty publish date list
+for web in webs:
+
+    # "try" in case there is no web link
+    try:
+        source = requests.get(web).text # access to the page and only extrat text
+        soup = BeautifulSoup(source, 'lxml') # soupfy the page
+        web_str = str(soup) # convert to string for 'regular expression' search
+    except requests.ConnectionError:
+        web_str = None
+
+    # "try" in case the page misses the publish date
+    try:
+        publish_date = pattern.search(web_str).group(0) # use brutal force to find the first match
+    except:
+        publish_date = None
+
+    print(publish_date)
+
+
+    dates_published.append(publish_date) # append the date list
+
+df['publish_date'] = dates_published # add the publish_date column
+
+df.to_csv('new_ccmf.csv')
